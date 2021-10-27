6e95ef467a1b7e447e467ea730ef0c3ab20c6b7a
QIu2386 <411908168@qq.com>
2021-10-27 09:35:42 +0800

    第4次提交

--- src/main/scala/com/spark/WordCount.scala
+++ src/main/scala/com/spark/WordCount.scala
@@ -1,5 +1,6 @@
 package com.spark
 
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 object WordCount {
@@ -9,15 +10,15 @@ object WordCount {
 
         val sc = new SparkContext(wordcount)
 
-//      val word: RDD[(String, Int)] = sc.textFile("C:\\Users\\Tong\\Desktop\\wordcount.txt").flatMap(_.split(" ")).map((_,1)).reduceByKey(_+_)
+      val word: RDD[(String, Int)] = sc.textFile("C:\\Users\\Tong\\Desktop\\498.txt").flatMap(_.split(" ")).map((_,1)).reduceByKey(_+_)
 
-//      word.foreach(println)
+      word.foreach(println)
 
-//        sc.textFile("/home/atguigu/wordcount.txt").flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _).saveAsTextFile("/home/atguigu/sad55fdlv54")
+//      sc.textFile("/home/atguigu/wordcount.txt").flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _).saveAsTextFile("/home/atguigu/sad55fdlv54")
 
 
-        val list=sc.makeRDD(List(1,2,3,4,5,6))
-        list.saveAsTextFile("/home/atguigu/711")
+//        val list=sc.makeRDD(List(1,2,3,4,5,6))
+//        list.saveAsTextFile("/home/atguigu/711")
 
         sc.stop()
 
